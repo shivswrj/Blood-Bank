213a56ca9921ea1edba7cfcd0ceb771c4e42a68b
Stuti Jain <stutijain241@gmail.com>
2024-01-17 12:33:08 +0530

    sidebar and popup modal

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Spinner from '../components/shared/Spinner'
-import { toast } from 'react-toastify'
-import Layout from '../components/shared/Layout/Layout'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Spinner from '../components/shared/Spinner';
+import { toast } from 'react-toastify';
+import Layout from '../components/shared/Layout/Layout';
+import Modal from '../components/shared/modal/Modal';
 const Home = () => {
-  const {loading,error} = useSelector((state)=>state.auth)
+  const {loading,error} = useSelector((state)=>state.auth);
   return (
     <Layout>
-      {error && <span>{toast(error)}</span>}
-      {loading?<Spinner/> : (
+      {error && <span>{alert (error)}</span>}
+      {loading?(<Spinner/> : 
         <div>
           HOME
         </div>    
-      )}
-    </Layout>
-  )
-}
+      ): (
+        <>
+      <h4 className="ms-4"  
+      data-bs-toggle="modal" 
+      data-bs-target="#staticBackdrop"
+      style = {{ cursor : "pointer"}}
+      >
 
-export default Home
+        <i className="fa-solid fa-plus text-success py-4"></i>
+        Add Inventory
+
+
+      </h4>
+      <Modal/>
+        </>)}
+    </Layout>
+  );
+};
+
+export default Home;
 
     
--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -15,6 +15,10 @@ const inventorySchema = new mongoose.Schema({
         type:Number,
         require:[true,"blood quantity is require"],
     },
+    donarEmail:{
+        type:String,
+        required :[true,"Donar Email is Required"],
+    },
     organisation:{
         type:mongoose.Schema.Types.String,
         ref:'users',
@@ -31,9 +35,9 @@ const inventorySchema = new mongoose.Schema({
     donar:{
         type: mongoose.Schema.Types.String,
         ref:"users",
-        required: function () {
-            return this.inventoryType === "in";
-        },
+       // required: function () {
+       //     return this.inventoryType === "in";
+       // },
     },
 }, { timestamps : true});
 
